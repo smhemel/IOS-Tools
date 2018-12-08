4b739f97cc8805fbd9ad394f75735f1b62d98b8b
smhemel <sm_hemel@yahoo.com>
2018-12-08 10:45:17 +0600

    Expended Cell

---
+++ ExpandedCell.swift
@@ -0,0 +1,15 @@
+//
+//  DataModel.swift
+//  ExpandedCell
+//
+//  Created by S M HEMEL on 8/12/18.
+//  Copyright © 2018 S M HEMEL. All rights reserved.
+//
+
+import Foundation
+struct cellData {
+    var opened = Bool()
+    var title = String()
+    var sectionData = [String]()
+     
+}
